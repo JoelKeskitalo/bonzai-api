0b972b0316b1338db70f509ee0931d2bf340ebf6
J0113boy <keskitalo_6@hotmail.com>
2024-09-26 07:54:12 +0200

    getRoomById maybe done

---
+++ bonzai-api/functions/getRoomById/index.js
@@ -0,0 +1,38 @@
+const { dynamoDb } = require('../../database/db')
+
+module.exports.handler = async (event) => {
+    try {
+        const { roomId } = event.pathParameters
+
+        if (!roomId) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Missing room id'})
+            }
+        }
+
+        const roomParams = {
+            TableName: 'Bonzai-Rooms',
+            Key: roomId
+        }
+
+        const result = await dynamoDb.scan(roomParams)
+        const oneRoom = result.Items
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Room found: ',
+                room: oneRoom
+            })
+        }
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        }
+    }
+}
